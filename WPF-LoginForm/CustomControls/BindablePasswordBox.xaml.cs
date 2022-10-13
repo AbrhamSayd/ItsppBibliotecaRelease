d06d04c3a12a4392b2f03001d30d3493e844457c
Darien Alejandro Verdugo <alexhid14@gmail.com>
2022-10-12 19:02:04 -0700

    Ligeros cambios al diseño Modificacion y creacion de Custom Controls

--- WPF-LoginForm/CustomControls/BindablePasswordBox.xaml.cs
+++ WPF-LoginForm/CustomControls/BindablePasswordBox.xaml.cs
@@ -41,5 +41,7 @@ namespace WPFBiblioteca.CustomControls
         {
             Password = txtPassword.SecurePassword;
         }
+
+
     }
 }
--- WPF-LoginForm/CustomControls/cdvEnterUser.xaml.cs
+++ WPF-LoginForm/CustomControls/cdvEnterUser.xaml.cs
@@ -10,23 +10,19 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WPFBiblioteca.Views
+namespace WPFBiblioteca.CustomControls
 {
     /// <summary>
-    /// Interaction logic for MainWindowView.xaml
+    /// Lógica de interacción para cdvEnterUser.xaml
     /// </summary>
-    public partial class MainView : Window
+    public partial class cdvEnterUser : UserControl
     {
-        public MainView()
+        public cdvEnterUser()
         {
             InitializeComponent();
-        }
-
-        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            this.DragMove();
         }
     }
 }
