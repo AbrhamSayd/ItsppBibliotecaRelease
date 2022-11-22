e9ccb0b26ff3d4742572a6f80fe7ed56d318d659
AbrhamSayd <abrham.said@gmail.com>
2022-11-21 21:25:13 -0700

    correciones

--- WPF-LoginForm/Repositories/ILendingRepository.cs
+++ WPF-LoginForm/Repositories/ILendingRepository.cs
@@ -9,8 +9,8 @@ public interface ILendingRepository
     Task<string> Add(LendingModel lending, UserModel currentUser);
     Task<string> Edit(LendingModel lending, int lendingId);
     Task<string> Delete(int lendingId);
-    Task<MemberModel> GetByMemberId(int memberId);
-    Task<MemberModel> GetByBook(int bookId);
+    Task<IEnumerable<LendingModel>> GetByMemberId(int memberId);
+    Task<IEnumerable<LendingModel>> GetByBook(int bookId);
     Task<IEnumerable<LendingModel>> GetByAll();
     string GetError();
 }
